7d522bd68638509839a54ab3ad35e37e89c31ec3
fadhlihh <fadhlimorth5@gmail.com>
2020-05-20 14:16:56 +0700

    Frontend 90% done

--- info.php
+++ info.php
@@ -8,18 +8,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-    <div class="header">
-
-    </div>
     <div class="content">
-        <div class="choice">
-            <a href="/covin" class="active">Dunia</a>
-            <a href="#">Indonesia</a>
-        </div>
+        <h1>Provinsi</h1>
         <table class="table table-bordered table-hover">
             <thead class="thead-light">
                 <tr>
-                    <th width="40%">Negara</th>
+                    <th width="40%">Kota</th>
                     <th width="20%">Positif</th>
                     <th width="20%">Sembuh</th>
                     <th width="20%">Meninggal</th>
@@ -52,6 +46,14 @@
                 </tr>
             </tbody>
         </table>
+    </div>
+    <div class="recommend">
+        <h6>Recommendation</h6>
+        <br>
+        <a href="#">Kota 1</a>
+        <a href="#">Kota 2</a>
+        <a href="#">Kota 3</a>
+        <br>
     </div>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
